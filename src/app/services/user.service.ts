6e70a6246569db9d479b56e6ec0341da736280ba
TannerB <tannerpace@gmail.com>
2021-06-21 11:43:32 -0400

    added functions to user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { User } from 'models/user.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  activeUser:User;
+
+  newActiveUser$: Subject<User> = new Subject<User>();
+
   constructor() { }
-}
+
+  setActiveUser(user: User){
+    this.activeUser = user;
+    this.newActiveUser$.next(this.activeUser);
+  }
+
+  getActiveUser(): User {
+    return this.activeUser
+  }
+};
