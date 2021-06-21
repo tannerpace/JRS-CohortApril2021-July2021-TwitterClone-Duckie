9f51b0be10798227e59c327ce1e569ba77fc51d7
TannerB <tannerpace@gmail.com>
2021-06-21 13:59:13 -0400

    add user model.ts match sql table duckie.user

--- models/user.model.ts
+++ models/user.model.ts
@@ -18,15 +18,3 @@ export class User {
 
 };
 
-id int unique not null auto_increment,
-    userName varchar(25) unique not null,
-    password varchar(255) not null,
-    screenName varchar(50) not null,
-    bio varchar(255),
-    website varchar(255),
-    birthDate Date not null,
-    dateJoined Datetime not null DEFAULT (current_date()),
-    quackCount int not null default 0,
-    -- profile pi img 
-    -- banner img
-    primary key (id)
