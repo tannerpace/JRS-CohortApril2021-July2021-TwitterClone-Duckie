7eb05f23e40d6e6b9e1c6ef2a6a917c43002d593
AlexDrew-sudo <alexdrew10@icloud.com>
2021-06-22 14:51:46 -0400

    Added createQuack, add Like&repost; deleteQuack functions to backend

--- src/servers/duckieServer/app/routes/duckie.routes.js
+++ src/servers/duckieServer/app/routes/duckie.routes.js
@@ -4,4 +4,9 @@ module.exports = app => {
     app.get("/", duckie.welcome);
     app.get("/api/user/:username", duckie.getUser);
     app.get("/api/createUser", duckie.createUser);
+    app.post("/api/tweet/", duckie.createQuack);
+    app.put('/api/tweet/like', duckie.addLike);
+    app.put('/api/tweet/repost', duckie.addRepost)
+    app.delete("/api/tweet/:id", duckie.deleteQuack);
+
 };
