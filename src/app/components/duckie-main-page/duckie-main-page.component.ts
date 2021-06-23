3f4b182e731e3dad843c3453de849f7f83598ed9
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-06-23 09:59:37 -0400

    more css on user page

--- src/app/components/duckie-main-page/duckie-main-page.component.ts
+++ src/app/components/duckie-main-page/duckie-main-page.component.ts
@@ -4,18 +4,16 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'duckie-main-page',
   templateUrl: './duckie-main-page.component.html',
-  styleUrls: ['./duckie-main-page.component.css']
+  styleUrls: ['./duckie-main-page.component.css'],
 })
 export class DuckieMainPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   buttonClicked() {
-    let user = {username: "blake", screenName: "screen name"}
-    console.log("clicked");
+    let user = { username: 'blake', screenName: 'screen name' };
+    console.log('clicked');
     this.router.navigate(['/user', user.username]);
   }
 }
