b45d11694c1e5cf85b30ef99b13d897108e74435
BlakeIngle <blake.ingle7@gmail.com>
2021-06-23 09:54:10 -0400

    merge 0.0.2

---
+++ src/servers/duckieServer/app/config/db.config.js
@@ -0,0 +1,5 @@
+// username and password are your username and password for your mysql connection
+exports.host = 'localhost';
+exports.user = '';
+exports.password = '';
+exports.database = 'duckie';
