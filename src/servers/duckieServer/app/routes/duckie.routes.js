b0c9c807a756bd1d3c61fd00287afb9766806109
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-06-28 11:27:03 -0400

    refactor: 'tweet' -> 'quack'

--- src/servers/duckieServer/app/routes/duckie.routes.js
+++ src/servers/duckieServer/app/routes/duckie.routes.js
@@ -1,31 +1,33 @@
-module.exports = app => {
-    const duckie = require("../controllers/duckie.controller");
+module.exports = (app) => {
+  const duckie = require("../controllers/duckie.controller");
 
-    app.get("/", duckie.welcome);
+  app.get("/", duckie.welcome);
 
-    // users
-    app.get("/api/user/id/:id", duckie.getUserById);
-    app.get("/api/user/:userName", duckie.getUser);
-    app.post("/api/user", duckie.createUser);
-    app.put("/api/user/:id", duckie.editUserInfo);
-    app.delete("/api/user/:id", duckie.deleteUser);
+  // users
+  app.get("/api/user/id/:id", duckie.getUserById);
+  app.get("/api/user/:userName", duckie.getUser);
+  app.post("/api/user", duckie.createUser);
+  app.put("/api/user/:id", duckie.editUserInfo);
+  app.delete("/api/user/:id", duckie.deleteUser);
 
-    app.post("/api/user/login", duckie.login);
+  app.post("/api/user/login", duckie.login);
 
-    app.post("/api/getQuacks", duckie.getQuacksByUser)
-    app.post("/api/getqb", duckie.getReposts)
-    app.post("/api/getlikes", duckie.getLikes)
-    app.post("/api/tweet/", duckie.createQuack);
-    app.put('/api/tweet/like', duckie.addLike);
-    app.put('/api/tweet/repost', duckie.addRepost)
-    app.delete("/api/tweet/:id", duckie.deleteQuack);
+  app.post("/api/getQuacks", duckie.getQuacksByUser);
+  app.post("/api/getqb", duckie.getReposts);
+  app.post("/api/getlikes", duckie.getLikes);
+  app.post("/api/quack/", duckie.createQuack);
+  app.put("/api/quack/like", duckie.addLike);
+  app.put("/api/quack/repost", duckie.addRepost);
+  app.delete("/api/quack/:id", duckie.deleteQuack);
 
-    app.post("/api/quack/reply", duckie.quackReply)
-    app.post("/api/followedquacks",duckie.getFollowingQuacks);
-    app.post("/api/following", duckie.getFollowingUser);
-    app.post("/api/followers", duckie.getFollowersUser);
+  app.post("/api/quack/reply", duckie.quackReply);
+  app.post("/api/followedquacks", duckie.getFollowingQuacks);
+  app.post("/api/following", duckie.getFollowingUser);
+  app.post("/api/followers", duckie.getFollowersUser);
 
-    app.post("/api/:followerId/follow/:userToFollowId", duckie.followUser);
-    app.delete("/api/:followerId/unfollow/:userToUnfollowId", duckie.unfollowUser);
-
+  app.post("/api/:followerId/follow/:userToFollowId", duckie.followUser);
+  app.delete(
+    "/api/:followerId/unfollow/:userToUnfollowId",
+    duckie.unfollowUser
+  );
 };
