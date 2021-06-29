9cf4a8508ecd37dd4396357889d89b4e20cd9aeb
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-06-28 21:03:18 -0400

    css fixes for profile pics and margin issues

--- src/app/components/compose/new-quack-form/new-quack-form.component.ts
+++ src/app/components/compose/new-quack-form/new-quack-form.component.ts
@@ -1,25 +1,51 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { Quack } from 'src/app/models/quack.model';
+import { QuackApiService } from 'src/app/services/quack-api.service';
 
 @Component({
   selector: 'new-quack-form',
   templateUrl: './new-quack-form.component.html',
-  styleUrls: ['./new-quack-form.component.css']
+  styleUrls: ['./new-quack-form.component.css'],
 })
 export class NewQuackFormComponent implements OnInit {
-
-  quackBody: string = "";
   @Input() user: User;
+  quack: Quack;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private quackApiService: QuackApiService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.user = this.userService.getActiveUser();
+    this.quack = new Quack({ userId: this.user.id, body: '' });
+    console.log(this.user);
   }
 
-
-
-  submitQuack(){
-    //adds new quack to database 
+  reRoutePage() {
+    this.router.navigate(['/home']);
   }
 
+  submitQuack() {
+    if (this.quack.body == '') {
+      console.log('No Quack Entered');
+      return;
+    }
+    console.log('posting');
+    this.quackApiService.createQuack(this.quack.body, this.user.id).subscribe(
+      (response) => {
+        console.log('New Quack Created');
+        this.userService.updateActiveUser();
+        this.reRoutePage();
+      },
+      (error) => {
+        alert('Quack was not created');
+      }
+    );
+  }
 }
