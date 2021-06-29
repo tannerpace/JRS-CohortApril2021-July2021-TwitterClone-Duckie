e5c8b5234c15d817ed497f5f1858c04960b8b5ec
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-06-29 11:14:50 -0400

    edit profile page loads user correctly, create tweet sends/recieves req body correctly

--- src/app/components/edit-user/edit-user-page/edit-user-page.component.ts
+++ src/app/components/edit-user/edit-user-page/edit-user-page.component.ts
@@ -5,27 +5,26 @@ import { User } from 'src/app/models/user.model';
 @Component({
   selector: 'app-edit-user-page',
   templateUrl: './edit-user-page.component.html',
-  styleUrls: ['./edit-user-page.component.css']
+  styleUrls: ['./edit-user-page.component.css'],
 })
 export class EditUserPageComponent implements OnInit {
-
   @Input() user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.data
-      .subscribe(user => {
-        if(user) {
-          this.user = user
-          console.log(user)
+    this.route.data.subscribe(
+      (data) => {
+        if (data[0]) {
+          this.user = data[0];
+          console.log(data[0]);
         } else {
           this.user = null;
         }
       },
-      error => {
+      (error) => {
         this.user = null;
-      });
+      }
+    );
   }
-
 }
