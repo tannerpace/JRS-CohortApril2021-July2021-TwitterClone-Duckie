40b5953923ac2cf3bea33139a9cbaec7a63f63d1
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-06-28 20:59:17 -0400

    updated model to include more properties

--- src/app/models/quack.model.ts
+++ src/app/models/quack.model.ts
@@ -1,7 +1,10 @@
 export class Quack {
     public id?: number;
-
     public userId?: number;
+    public dateAndTime?: Date;
+    public repostCount?:number;
+    public likeCount?:number;
+    public replyTo?:number;
     public body?: string;
 
     constructor(obj: any) {
