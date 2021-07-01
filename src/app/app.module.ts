5315e216de13d2e980ad7df8d325938ffde2c1d4
JazminCarino <62260985+JazminCarino@users.noreply.github.com>
2021-07-01 12:38:39 -0400

    added profile-pic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,14 @@ import { LoginPageComponent } from './components/login/login-page/login-page.com
 import { LoginFormComponent } from './components/login/login-form/login-form.component';
 import { EditUserFormComponent } from './components/edit-user/edit-user-form/edit-user-form.component';
 import { NewQuackFormComponent } from './components/compose/new-quack-form/new-quack-form.component';
-import { QuackFeedComponent } from './components/quack-feed/quack-feed.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NewQuackPageComponent } from './components/compose/new-quack-page/new-quack-page.component';
 import { CreateUserFormComponent } from './components/login/create-user-form/create-user-form.component';
 import { EditUserPageComponent } from './components/edit-user/edit-user-page/edit-user-page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { QuackCardComponent } from './components/quack-card/quack-card.component';
+import { ProfilePicComponent } from './components/profile-pic/profile-pic.component';
+import { QuackFeedComponent } from './components/quack-feed/quack-feed.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { QuackCardComponent } from './components/quack-card/quack-card.component
     NewQuackPageComponent,
     EditUserPageComponent,
     NotFoundComponent,
-    QuackCardComponent
+    QuackCardComponent,
+    ProfilePicComponent
   ],
   imports: [
     BrowserModule,
