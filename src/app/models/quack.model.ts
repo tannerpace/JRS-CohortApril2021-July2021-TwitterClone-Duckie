bb9af5e2755bd847575d574e7ccbbd6f30718447
TannerB <tannerpace@gmail.com>
2021-07-01 14:23:38 -0400

    Quack  to include profilePic,userName, screenName.

--- src/app/models/quack.model.ts
+++ src/app/models/quack.model.ts
@@ -1,11 +1,17 @@
 export class Quack {
     public id?: number;
     public userId?: number;
-    public dateAndTime?: Date;
-    public repostCount?:number;
-    public likeCount?:number;
-    public replyTo?:number;
+    public screenName?: string;
+    public userName?: string;
+    public profilePic?: string;
+
     public body?: string;
+
+    public dateAndTime?: Date;
+    public repostCount?: number;
+    public likeCount?: number;
+    public replyTo?: number;
+
 
     constructor(obj: any) {
         Object.assign(this, obj);
